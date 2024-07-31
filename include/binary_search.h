f1840e0343d8e546bc17fd13df7d449c1e4a9d08
fengjiran <fengjiran@foxmail.com>
2024-07-31 15:16:04 +0800

    test binary search

---
+++ include/binary_search.h
@@ -0,0 +1,68 @@
+//
+// Created by richard on 7/31/24.
+//
+
+#ifndef ATYPICALLIBRARY_BINARY_SEARCH_H
+#define ATYPICALLIBRARY_BINARY_SEARCH_H
+
+#include <vector>
+
+template<typename T>
+class BinarySearch {
+public:
+    /**
+     * @brief Find the index of the first element that greater equal the target.
+     * @param nums Ascending array.
+     * @param left Left bound of search interval.
+     * @param right Right bound of search interval.
+     * @param target Target.
+     * @return Index.
+     */
+    static int FindFirstGEItem(const std::vector<T>& nums, int left, int right, T target) {
+        int l = left;
+        int r = right + 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        if (l == right + 1) {
+            return -1;
+        }
+
+        return l;
+    }
+
+    /**
+     * @brief Find the index of the first element that greater than the target.
+     * @param nums Ascending array.
+     * @param left Left bound of search interval.
+     * @param right Right bound of search interval.
+     * @param target Target.
+     * @return Index.
+     */
+    static int FindFirstGTItem(const std::vector<T>& nums, int left, int right, T target) {
+        int l = left;
+        int r = right + 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] <= target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        if (l == right + 1) {
+            return -1;
+        }
+
+        return l;
+    }
+};
+
+#endif//ATYPICALLIBRARY_BINARY_SEARCH_H
---
+++ test/binary_search_test.cpp
@@ -0,0 +1,21 @@
+//
+// Created by richard on 7/31/24.
+//
+
+#include "binary_search.h"
+#include "gtest/gtest.h"
+
+TEST(BinarySearch, t1) {
+    std::vector<int> nums1{2, 4, 5, 6, 9};
+    EXPECT_EQ(BinarySearch<int>::FindFirstGEItem(nums1, 0, nums1.size() - 1, 2), 0);
+    EXPECT_EQ(BinarySearch<int>::FindFirstGEItem(nums1, 0, nums1.size() - 1, 1), 0);
+    EXPECT_EQ(BinarySearch<int>::FindFirstGEItem(nums1, 0, nums1.size() - 1, 9), 4);
+    EXPECT_EQ(BinarySearch<int>::FindFirstGEItem(nums1, 0, nums1.size() - 1, 10), -1);
+
+    EXPECT_EQ(BinarySearch<int>::FindFirstGTItem(nums1, 0, nums1.size() - 1, 2), 1);
+    EXPECT_EQ(BinarySearch<int>::FindFirstGTItem(nums1, 0, nums1.size() - 1, 1), 0);
+    EXPECT_EQ(BinarySearch<int>::FindFirstGTItem(nums1, 0, nums1.size() - 1, 9), -1);
+
+    std::vector<int> nums2{2, 3, 3, 4, 5, 6, 9};
+    EXPECT_EQ(BinarySearch<int>::FindFirstGEItem(nums2, 0, nums2.size() - 1, 10), -1);
+}
