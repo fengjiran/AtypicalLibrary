9079bbb6d86ce7724ee6818f1e17bb6c86a3e239
fengjiran <fengjiran@foxmail.com>
2024-08-29 02:04:22 +0800

    add shared_ptr for atp

---
+++ include/atl_shared_ptr.hpp
@@ -0,0 +1,17 @@
+//
+// Created by richard on 8/28/24.
+//
+
+#ifndef ATYPICALLIBRARY_ATL_SHARED_PTR_HPP
+#define ATYPICALLIBRARY_ATL_SHARED_PTR_HPP
+
+namespace atp {
+
+template<typename T>
+class shared_ptr {
+
+};
+
+}
+
+#endif//ATYPICALLIBRARY_ATL_SHARED_PTR_HPP
---
+++ test/atl_shared_ptr_test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by richard on 8/28/24.
+//
+#include "atl_shared_ptr.hpp"
+#include "gtest/gtest.h"
