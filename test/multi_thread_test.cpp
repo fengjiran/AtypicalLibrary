8ee73622a6c96b7c1e04674e90ed8d67a458b511
fengjiran <fengjiran@foxmail.com>
2024-09-30 11:28:33 +0800

    test multi thread

--- test/multi_thread_test.cpp
+++ test/multi_thread_test.cpp
@@ -11,4 +11,26 @@ TEST(MultiThreadTest, test1) {
         std::cout << "Hello Concurrent World\n";
         std::cout << "concurrency num: " << std::thread::hardware_concurrency() << std::endl;
     };
+
+    std::thread t1(f);
+    std::thread t2(f);
+
+    if (t1.joinable()) {
+        t1.join();
+    }
+
+    if (t2.joinable()) {
+        t2.join();
+    }
+}
+
+TEST(MultiThreadTest, test2) {
+    auto f = [](int i, int j) {
+        std::cout << i + j << std::endl;
+    };
+
+    for (int i = 0; i < 4; ++i) {
+        std::thread t(f, i, i);
+        t.detach();
+    }
 }
