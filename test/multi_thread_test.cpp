abe0f7c20fbef2e3a65e904f62f778aecfb50f20
fengjiran <fengjiran@foxmail.com>
2024-10-01 00:16:15 +0800

    test multi thread

--- test/multi_thread_test.cpp
+++ test/multi_thread_test.cpp
@@ -37,7 +37,7 @@ TEST(MultiThreadTest, test2) {
 }
 
 TEST(MultiThreadTest, test3) {
-    auto f1 = [](int& n) {
+    auto f1 = [](int n) {
         std::cout << "thread id: " << std::this_thread::get_id() << std::endl;
         for (int i = 0; i < 5; ++i) {
             std::cout << "Thread 1 is executing\n";
@@ -46,7 +46,7 @@ TEST(MultiThreadTest, test3) {
         }
     };
 
-    auto f2 = [](int n) {
+    auto f2 = [](int& n) {
         std::cout << "thread id: " << std::this_thread::get_id() << std::endl;
         for (int i = 0; i < 5; ++i) {
             std::cout << "Thread 2 is executing\n";
@@ -55,19 +55,34 @@ TEST(MultiThreadTest, test3) {
         }
     };
 
+    auto f3 = [](const int* p) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << *p << std::endl;
+    };
+
     int n = 0;
-    int n1 = n + 1;
-    std::thread t1;
-    std::thread t2(f1, std::ref(n1));      // pass by value
+
+    std::thread t2(f1, n + 1);      // pass by value
     std::thread t3(f2, std::ref(n));// pass by reference
     std::thread t4(std::move(t3));  // t4 is now running f2, t3 is no longer a thread
 
+    int x = 1;
+    std::thread t1(f3, &x);
+    t1.detach();
+
+    // if (t1.joinable()) {
+    //     // t1.join();
+    //     t1.detach();
+    // }
+
     if (t2.joinable()) {
         t2.join();
+        // t2.detach();
     }
 
     if (t4.joinable()) {
         t4.join();
+        // t4.detach();
     }
 
     std::cout << "Final value of n is " << n << std::endl;
