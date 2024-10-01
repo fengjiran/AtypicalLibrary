06dd20d15a81b1d22f5216392f2eafc78f2124f2
fengjiran <fengjiran@foxmail.com>
2024-10-01 15:24:27 +0800

    test multi thread

--- test/multi_thread_test.cpp
+++ test/multi_thread_test.cpp
@@ -8,13 +8,17 @@
 #include <thread>
 
 TEST(MultiThreadTest, test1) {
-    auto f = []() {
+    auto f1 = [] {
         std::cout << "Hello Concurrent World\n";
         std::cout << "concurrency num: " << std::thread::hardware_concurrency() << std::endl;
     };
 
-    std::thread t1(f);
-    std::thread t2(f);
+    auto f2 = [](int i, int j) {
+        std::cout << i + j << std::endl;
+    };
+
+    std::thread t1(f1);
+    std::thread t2(f1);
 
     if (t1.joinable()) {
         t1.join();
@@ -23,20 +27,14 @@ TEST(MultiThreadTest, test1) {
     if (t2.joinable()) {
         t2.join();
     }
-}
-
-TEST(MultiThreadTest, test2) {
-    auto f = [](int i, int j) {
-        std::cout << i + j << std::endl;
-    };
 
     for (int i = 0; i < 4; ++i) {
-        std::thread t(f, i, i);
+        std::thread t(f2, i, i);
         t.detach();
     }
 }
 
-TEST(MultiThreadTest, test3) {
+TEST(MultiThreadTest, test2) {
     auto f1 = [](int n) {
         std::cout << "thread id: " << std::this_thread::get_id() << std::endl;
         for (int i = 0; i < 5; ++i) {
@@ -56,34 +54,53 @@ TEST(MultiThreadTest, test3) {
     };
 
     auto f3 = [](const int* p) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        std::cout << *p << std::endl;
+        // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << "The value is: " << *p << std::endl;
     };
 
     int n = 0;
+    std::thread t1(f1, n + 1);
+    std::thread t2(f2, std::ref(n));
+    std::thread t3(std::move(t2));
 
-    std::thread t2(f1, n + 1);      // pass by value
-    std::thread t3(f2, std::ref(n));// pass by reference
-    std::thread t4(std::move(t3));  // t4 is now running f2, t3 is no longer a thread
+    // int x = 1;
+    int* x = new int(1);
+    std::thread t4(f3, x);
+    // delete x;
 
-    int x = 1;
-    std::thread t1(f3, &x);
-    t1.detach();
+    if (t1.joinable()) {
+        t1.join();
+    }
 
-    // if (t1.joinable()) {
-    //     // t1.join();
-    //     t1.detach();
-    // }
-
-    if (t2.joinable()) {
-        t2.join();
-        // t2.detach();
+    if (t3.joinable()) {
+        t3.join();
     }
 
     if (t4.joinable()) {
-        t4.join();
         // t4.detach();
+        t4.join();
     }
+    delete x;
 
     std::cout << "Final value of n is " << n << std::endl;
+}
+
+TEST(MultiThreadTest, test3) {
+    int data = 0;
+    std::mutex mtx;
+
+    auto f = [&data, &mtx] {
+        for (int i = 0; i < 5000; ++i) {
+            mtx.lock();
+            data++;
+            mtx.unlock();
+        }
+    };
+
+    std::thread t1(f);
+    std::thread t2(f);
+    t1.join();
+    t2.join();
+    // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout << "data = " << data << std::endl;
 }
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,8 +1,8 @@
 //
 // Created by richard on 4/29/24.
 //
-#include "gtest/gtest.h"
 #include "glog/logging.h"
+#include "gtest/gtest.h"
 
 int main() {
     testing::InitGoogleTest();
