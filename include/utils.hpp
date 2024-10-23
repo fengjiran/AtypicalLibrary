91574420b692bde2e8bce9383561b0e4f8e5733f
fengjiran <fengjiran@foxmail.com>
2024-10-23 21:53:39 +0800

    utils

--- include/utils.hpp
+++ include/utils.hpp
@@ -23,29 +23,29 @@ using void_t = void;
 
 template<typename T>
 T* to_address(T* p) noexcept {
-    static_assert(!std::is_function<T>::value, "value is a function type");
+    static_assert(!std::is_function_v<T>, "value is a function type");
     return p;
 }
 
 template<typename Iter, typename value_type>
-using has_input_iterator_category = typename std::enable_if<
-        std::is_convertible<typename std::iterator_traits<Iter>::iterator_category, std::input_iterator_tag>::value &&
-                std::is_constructible<value_type, typename std::iterator_traits<Iter>::reference>::value,
+using has_input_iterator_category = std::enable_if<
+        std::is_convertible_v<typename std::iterator_traits<Iter>::iterator_category, std::input_iterator_tag> &&
+                std::is_constructible_v<value_type, typename std::iterator_traits<Iter>::reference>,
         int>;
 
 template<typename T, T v>
 struct integral_constant {
-    static constexpr const T value = v;
+    static constexpr T value = v;
     using value_type = T;
     using type = integral_constant;
-    inline constexpr explicit operator value_type() const noexcept { return value; }
-    inline constexpr value_type operator()() const noexcept { return value; }
+    constexpr explicit operator value_type() const noexcept { return value; }
+    constexpr value_type operator()() const noexcept { return value; }
 };
 
 using true_type = integral_constant<bool, true>;
 using false_type = integral_constant<bool, false>;
 
-template<typename alloc, typename = void>
+template<typename, typename = void>
 struct _has_select_on_container_copy_construction : false_type {};
 
 template<typename alloc>
@@ -54,15 +54,15 @@ struct _has_select_on_container_copy_construction<
         decltype((void) std::declval<alloc>().select_on_container_copy_construction())> : true_type {};
 
 template<typename alloc,
-         typename = typename std::enable_if<_has_select_on_container_copy_construction<const alloc>::value>::type>
-inline constexpr static alloc select_on_container_copy_construction(const alloc& a) {
+         typename = std::enable_if_t<_has_select_on_container_copy_construction<const alloc>::value>>
+constexpr static alloc select_on_container_copy_construction(const alloc& a) {
     return a.select_on_container_copy_construction();
 }
 
 template<typename alloc,
          typename = void,
-         typename = typename std::enable_if<!_has_select_on_container_copy_construction<const alloc>::value>::type>
-inline constexpr static alloc select_on_container_copy_construction(const alloc& a) {
+         typename = std::enable_if_t<!_has_select_on_container_copy_construction<const alloc>::value>>
+constexpr static alloc select_on_container_copy_construction(const alloc& a) {
     return a;
 }
 
