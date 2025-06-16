48e95f0b720f935cb7844546633647e6336a18ae
fengjiran <fengjiran@foxmail.com>
2025-06-16 10:29:14 +0800

    add tensor

--- include/tensor.h
+++ include/tensor.h
@@ -5,8 +5,28 @@
 #ifndef TENSOR_H
 #define TENSOR_H
 
-// #include <cstdint>
+#include <cstdint>
 #include <vector>
+
+#ifdef __has_cpp_attribute
+#if __has_cpp_attribute(nodiscard)
+#define NODISCARD [[nodiscard]]
+#else
+#define NODISCARD
+#endif
+
+#if __has_cpp_attribute(maybe_unused)
+#define MAYBE_UNUSED [[maybe_unused]]
+#else
+#define MAYBE_UNUSED
+#endif
+#endif
+
+#define UNUSED(expr)   \
+    do {               \
+        (void) (expr); \
+    } while (false)
+
 
 inline int32_t atomic_inc_relaxed(int32_t* ptr) {
     return __atomic_fetch_add(ptr, 1, __ATOMIC_RELAXED);
@@ -48,21 +68,34 @@ struct DLDataType {
     // Number of lanes per data element, for vector.
     int16_t lanes;
 };
-//
-// class TensorNode;
-//
-// inline size_t GetDataSize(const TensorNode& t) {
-//     size_t size = 1;
-//
-// }
 
+struct TensorInfo {
+    void* data;
+    int32_t ndim;
+    std::vector<int64_t> shape;
+    std::vector<int64_t> strides;
+    DLDataType dtype;
+    Device device;
+    int32_t ref_counter;
+};
+
+
+template<typename Allocator>
 class TensorNode {
 public:
     TensorNode() : data(nullptr), ndim(0), dtype({DLDataTypeCode::kFloat, 32, 1}),
                    device(DeviceType::kCPU), ref_counter(0) {}
 
-    TensorNode(std::vector<int64_t> shape, DLDataType dtype, DeviceType device)
+    TensorNode(Allocator alloc, std::vector<int64_t> shape, DLDataType dtype, DeviceType device)
         : shape(std::move(shape)) {}
+
+    NODISCARD int32_t use_count() const {
+        return atomic_load_relaxed(&ref_counter);
+    }
+
+    NODISCARD bool unique() const {
+        return use_count() == 1;
+    }
 
 private:
     void inc_ref() {
@@ -75,6 +108,7 @@ private:
     std::vector<int64_t> strides;
     DLDataType dtype;
     Device device;
+    Allocator alloc;
 
     int32_t ref_counter;
 };
