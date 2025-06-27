5004678bd98e43c7a2728ad2544ab0b335e666f4
fengjiran <fengjiran@foxmail.com>
2025-06-27 23:55:02 +0800

    alloc

--- include/storage.h
+++ include/storage.h
@@ -68,6 +68,45 @@ private:
     std::shared_ptr<StorageImpl> impl_;
 };
 
+// Packed container for TensorImpl shape and strides.
+// 1 size_t for the size
+// 5 eightbytes of inline sizes and 5 eightbytes of inline strides, OR pointer
+// to out-of-line array
+template<size_t MAX_INLINE_SIZE = 5>
+class ShapeAndStride {
+public:
+    NODISCARD size_t size() const noexcept {
+        return size_;
+    }
+
+    NODISCARD int64_t* shape_data() noexcept {
+        return is_inline() ? &inline_storage_[0] : &out_of_line_storage_[0];
+    }
+
+    NODISCARD const int64_t* shape_data() const noexcept {
+        return is_inline() ? &inline_storage_[0] : &out_of_line_storage_[0];
+    }
+
+    NODISCARD int64_t* stride_data() noexcept {
+        return is_inline() ? &inline_storage_[MAX_INLINE_SIZE] : &out_of_line_storage_[size()];
+    }
+
+    NODISCARD const int64_t* stride_data() const noexcept {
+        return is_inline() ? &inline_storage_[MAX_INLINE_SIZE] : &out_of_line_storage_[size()];
+    }
+
+private:
+    NODISCARD bool is_inline() const noexcept {
+        return size_ <= MAX_INLINE_SIZE;
+    }
+
+    size_t size_{1};
+    union {
+        int64_t* out_of_line_storage_;
+        int64_t inline_storage_[MAX_INLINE_SIZE * 2];
+    };
+};
+
 }// namespace atp
 
 #endif//STORAGE_H
