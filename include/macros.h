27f19316ef14bd2ae73141295f0f740bf2ec917b
fengjiran <fengjiran@foxmail.com>
2025-07-17 00:00:09 +0800

    add macros

--- include/macros.h
+++ include/macros.h
@@ -5,4 +5,41 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#endif //MACROS_H
+#ifdef __has_cpp_attribute
+#if __has_cpp_attribute(nodiscard)
+#define NODISCARD [[nodiscard]]
+#else
+#define NODISCARD
+#endif
+
+#if __has_cpp_attribute(maybe_unused)
+#define MAYBE_UNUSED [[maybe_unused]]
+#else
+#define MAYBE_UNUSED
+#endif
+#endif
+
+#if defined(__clang__)
+#define __ubsan_ignore_pointer_overflow__ __attribute__((no_sanitize("pointer-overflow")))
+#else
+#define __ubsan_ignore_pointer_overflow__
+#endif
+
+#if defined(__GNUC__)
+#define ATTRIBUTE_UNUSED __attribute__((unused))
+#else
+#define ATTRIBUTE_UNUSED
+#endif
+
+
+#define UNUSED(expr)   \
+    do {               \
+        (void) (expr); \
+    } while (false)
+
+#define STR_CONCAT_(__x, __y) __x##__y
+#define STR_CONCAT(__x, __y) STR_CONCAT_(__x, __y)
+
+#define REG_VAR_DEF static ATTRIBUTE_UNUSED uint32_t _make_unique_tid_
+
+#endif//MACROS_H
--- include/tensor_utils.h
+++ include/tensor_utils.h
@@ -5,46 +5,11 @@
 #ifndef TENSOR_UTILS_H
 #define TENSOR_UTILS_H
 
+#include "macros.h"
+
 #include <cstdint>
 #include <stdexcept>
 #include <string>
-
-#ifdef __has_cpp_attribute
-#if __has_cpp_attribute(nodiscard)
-#define NODISCARD [[nodiscard]]
-#else
-#define NODISCARD
-#endif
-
-#if __has_cpp_attribute(maybe_unused)
-#define MAYBE_UNUSED [[maybe_unused]]
-#else
-#define MAYBE_UNUSED
-#endif
-#endif
-
-#if defined(__clang__)
-#define __ubsan_ignore_pointer_overflow__ __attribute__((no_sanitize("pointer-overflow")))
-#else
-#define __ubsan_ignore_pointer_overflow__
-#endif
-
-#if defined(__GNUC__)
-#define ATTRIBUTE_UNUSED __attribute__((unused))
-#else
-#define ATTRIBUTE_UNUSED
-#endif
-
-
-#define UNUSED(expr)   \
-    do {               \
-        (void) (expr); \
-    } while (false)
-
-#define STR_CONCAT_(__x, __y) __x##__y
-#define STR_CONCAT(__x, __y) STR_CONCAT_(__x, __y)
-
-#define REG_VAR_DEF static ATTRIBUTE_UNUSED uint32_t _make_unique_tid_
 
 namespace atp {
 
